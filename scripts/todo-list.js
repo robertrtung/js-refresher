0443e59f5e6045f50f63cc2eac352094c28a8946
rtung1333 <gnuttrebor07@gmail.com>
2016-05-13 19:52:58 -0500

    Items can now be added via the input box

--- scripts/todo-list.js
+++ scripts/todo-list.js
@@ -2,7 +2,7 @@
  * To-do List
  */
 
-function extractFormValues(list) {
+function extractFormValues(list, itemText) {
     //var text = "";
     //var i;
     /*for (i = 0; i < x.length ;i++) {
@@ -11,12 +11,33 @@ function extractFormValues(list) {
     //text+=list.elements["task"].value+"<br>";
     //text += "<li>" + "Hello" + "</li>";
     var item = document.createElement("li");
-    item.innerText = "Hello";
+    item.innerText = itemText;
     list.appendChild(item);
     //document.getElementById("demo").append(text);
 }
 
+var inputBox = document.getElementById("task");
+inputBox.focus();
+
 var btnNew = document.getElementById("try");
 btnNew.onclick = function(){
-	extractFormValues(document.getElementById("list"));
+	var text = inputBox.value;
+	if(!text || text == "" || text == " "){
+		return;
+	}
+	extractFormValues(document.getElementById("list"),text);
+	inputBox.focus();
+	inputBox.select();
+};
+
+inputBox.onkeydown = function(e){
+	if(e.which == 13){//equals enter key
+		var text = inputBox.value;
+		if(!text || text == "" || text == " "){
+			return;
+		}
+		extractFormValues(document.getElementById("list"),text);
+		inputBox.focus();
+		inputBox.select();
+	}
 };
